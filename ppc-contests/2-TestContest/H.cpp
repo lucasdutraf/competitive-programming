1752b3aa0de5ed902e70a8d6143351806133fa84
Lucas Dutra <ldutra98@gmail.com>
2019-04-10 23:40:44 -0300

    Add ppc second contest H answer

--- ppc-contests/2-TestContest/H.cpp
+++ ppc-contests/2-TestContest/H.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using vi = vector<int>;
 int main () {
 
+  int conts, aux, ans = 0;
+  cin >> conts;
+  vi ls, ord;
+  while (conts--) {
+    cin >> aux;
+    ls.push_back(aux);
+  }
+
+  ord.push_back(ls[0]);
+  for (size_t i = 1; i < ls.size(); i++) {
+    if ((ls[i] == ord[ord.size() - 1]) || (ls[i] == ord[0])) {
+      ord.push_back(ls[i]);
+      sort(ord.begin(), ord.end());
+    }
+    else {
+      ord.push_back(ls[i]);
+      sort(ord.begin(), ord.end());
+      if ((ls[i] == ord[ord.size() - 1]) || (ls[i] == ord[0])) {
+        ans++;
+      }
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
