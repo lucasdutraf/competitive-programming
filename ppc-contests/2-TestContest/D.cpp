ed7cf0eebe5e4c8274f935bedab3a905b5920750
Lucas Dutra <ldutra98@gmail.com>
2019-04-12 14:04:04 -0300

    Add ppc second Contest D answer

--- ppc-contests/2-TestContest/D.cpp
+++ ppc-contests/2-TestContest/D.cpp
@@ -12,39 +12,50 @@ int main () {
     aux.push_back(us);
     s.push_back(us);
   }
-  mds = s;
   sort(aux.begin(), aux.end());
   gr = aux[aux.size() - 1];
   les = aux[0];
-  reverse(mds.begin(), mds.end());
-  for (size_t i = 0; i < s.size(); i++) {
-    if (s[i] == les) {
-      posles = i;
-    }
-  }
-  posles = s.size() - (posles + 1);
+
   for (size_t j = 0; j < s.size(); j++) {
     if (s[j] == gr) {
       posgr = j;
+      break;
     }
   }
   while (s[0] != gr) {
-    if (posgr == s[0]) {
+    cerr << "greater - s[" << posgr << "]: "<< s[posgr] << endl;
+    cerr << "comeco greater - num: " << nums << endl;
+    if (gr == s[0]) {
+      cerr << "greater dentro - s[" << posgr << "]: "<< s[posgr] << endl;
+      cerr << "entrou memo: " << endl;
       break;
     }
     swap(s[posgr - 1], s[posgr]);
+    cerr << "swap" << endl;
     nums++;
-    posgr -= 1;
+    posgr--;
+    cerr << "final greater - num: " << nums << endl;
   }
-  cerr << "aqui" << endl;
-  // while (s[s.size() - 1] != les) {
-  //   if (posles == s[s.size() - 1]) {
-  //     break;
-  //   }
-  //   swap(s[posles], s[posles + 1]);
-  //   nums++;
-  //   posles -= 1;
-  // }
+  mds = s;
+  reverse(mds.begin(), mds.end());
+  for (size_t i = 0; i < mds.size(); i++) {
+    if (mds[i] == les) {
+      posles = i;
+      break;
+    }
+  }
+  posles = s.size() - (posles + 1);
+  while (s[s.size() - 1] != les) {
+    cerr << "lesser - /s[" << posles << "]: "<< s[posles] << endl;
+    cerr << "comeco lesser - num: " << nums << endl;
+    if (les == s[s.size() - 1]) {
+      break;
+    }
+    swap(s[posles], s[posles + 1]);
+    nums++;
+    posles++;
+    cerr << "final lesser - num: " << nums << endl;
+  }
   cout << nums << endl;
   return 0;
 }
