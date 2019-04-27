f135c05a393c694dd0d08fee25a973617f248f8b
Lucas Dutra <ldutra98@gmail.com>
2019-04-26 22:36:50 -0300

    Add ppc third test exam G answer

--- ppc-contests/3-TestContest/G.cpp
+++ ppc-contests/3-TestContest/G.cpp
@@ -1,34 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-using di = deque<int>;
-using mii = map <int, int>;
+using pi = pair<int, int>;
+using di = deque<pi>;
 
 int main () {
-  int numkids, qtdcan, aux, ans;
-  di kids;
-  mii hl;
-  cin >> numkids >> qtdcan;
-  for (size_t i = 0; i < numkids; i++) {
+  int n, m, aux, pos = 0;
+  di res;
+  cin >> n >> m;
+  for (size_t i = 1; i <= n; i++) {
     cin >> aux;
-    kids.push_back(aux);
-    hl[i] = aux;
+    res.push_back(make_pair(aux, i));
   }
+  int a = 0;
   while (true) {
-    cerr << "oi" << endl;
-    if (kids.size() == 1) {
-      cerr << "tchau" << endl;
-      ans = kids.front();
+    if (res.size() == 1) {
+      pos = res.front().second;
       break;
     }
-    if (kids.front() <= qtdcan) {
-      kids.pop_front();
+    if (m >= res.front().first) {
+      res.pop_front();
     }
     else {
-      kids.push_back(kids.front());
-      kids.pop_front();
+      a = (res.front().first - m);
+      res.push_back(make_pair(a, res.front().second));
+      res.pop_front();
     }
   }
-  cout << ans << endl;
-
+  cout << pos << endl;
   return 0;
 }
