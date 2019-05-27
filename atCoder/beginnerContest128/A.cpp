760ca48346710f4cebdaf142bbef3251651f6fc9
Lucas Dutra <ldutra98@gmail.com>
2019-05-27 12:40:22 -0300

    Add atcoder beg Contest 128 A answer

---
+++ atCoder/beginnerContest128/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+  int ap, piecesap;
+  cin >> ap >> piecesap;
+  piecesap += ap * 3;
+  cout << piecesap/2 << endl;
+  return 0;
+}
